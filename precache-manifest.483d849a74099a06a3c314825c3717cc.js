efb2d6b683af784120bd31b17506cad6c634c5f2
astridx <info@astrid-guenther.de>
2020-06-30 17:46:44 +0200

    Updates

---
+++ precache-manifest.483d849a74099a06a3c314825c3717cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56ea9a81337de6eba95a1e6c52f1fd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5546c8b5105f0fe6a6a",
+    "url": "/static/css/main.6dcc3ae9.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5546c8b5105f0fe6a6a",
+    "url": "/static/js/main.6d66bcce.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
